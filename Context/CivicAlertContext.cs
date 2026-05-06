711dc6f043d52260ffc5224b94c0edaa75c49007
Vaduva Paula <paula.alexandra.vaduva@gmail.com>
2026-05-06 16:50:56 +0300

    add models

---
+++ Context/CivicAlertContext.cs
@@ -0,0 +1,19 @@
+﻿using CivicAlert.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CivicAlert.Context
+{
+    public class CivicAlertContext : IdentityDbContext<User>
+    {
+        public CivicAlertContext(DbContextOptions<CivicAlertContext> options) : base(options) { }
+
+        public DbSet<Category> Categories  { get; set; }
+        public DbSet<Issue> Issues { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ Models/Issue.cs
@@ -0,0 +1,33 @@
+﻿namespace CivicAlert.Models
+{
+    public enum IssueStatus
+    {
+        New,
+        Solved
+    }
+
+    public class Issue
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; } = string.Empty;
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
+        public IssueStatus Status { get; set; }
+
+        public string? ImageUrl { get; set; }
+        public int Severity { get; set; } = 1;
+
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+
+    }
+}
